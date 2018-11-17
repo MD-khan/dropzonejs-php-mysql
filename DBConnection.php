87060a9839fb871bfa8d9b95b19ed6c0c978d2b8
MD-khan <md.monir.khan707@gmail.com>
2018-11-17 18:19:13 -0500

    Update DBConnection.php

--- DBConnection.php
+++ DBConnection.php
@@ -4,7 +4,7 @@ class Dbconnection {
     
 	public function dbCon() {		
 		
-	   return new PDO("mysql:host=localhost; dbname=carbookingusa01761852273", "monirkarjumar", "hgljkgSfdFDSFsd%$%4532235fkdsGSRewrff");
+	   return new PDO("mysql:host=localhost; dbname=", "", "");
         
            
         }
